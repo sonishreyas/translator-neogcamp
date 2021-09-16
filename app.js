139f9c83996a0825e783ab1cfbe673058dd73b1f
Shreyas Soni <sonishreyas10@gmail.com>
2021-09-17 00:04:12 +0530

    style changes and connected to the dothraki api

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector('#output');
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json"
 
+function getTranslationURL(text)    {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error)    {
+    console.log("error occured", error)
+    alert("something wrong with server! try again after sometime")
+}
 function clickEventHandler() {
-    outputDiv.innerText = "translated!!!" +  txtInput.value
+    // outputDiv.innerText = "translated!!!" +  txtInput.value
+    var inputText = txtInput.value; //taking input
+    // calling server for processing
+    fetch(getTranslationURL(serverURL+"?"+inputText))
+    .then(response => response.json())
+    .then(json => console.log(json.contents.translated))
+    .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickEventHandler)
