86dda81c56664a3e1df718e9ecc4dee5a2480080
Shreyas Soni <sonishreyas10@gmail.com>
2021-09-17 00:29:56 +0530

    url changes from trial to dothraki

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector('#output');
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-// var serverURL = "https://api.funtranslations.com/translate/dothraki.json"
+var serverURL = "https://api.funtranslations.com/translate/dothraki.json"
 
 function getTranslationURL(text)    {
     return serverURL + "?" + "text=" + text
