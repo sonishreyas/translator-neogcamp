35b52e5e45bc35e79b91c84147571c42f6ddd551
Shreyas Soni <sonishreyas10@gmail.com>
2021-09-17 00:25:55 +0530

    output to div

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector('#output');
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-var serverURL = "https://api.funtranslations.com/translate/dothraki.json"
+// var serverURL = "https://api.funtranslations.com/translate/dothraki.json"
 
 function getTranslationURL(text)    {
     return serverURL + "?" + "text=" + text
@@ -13,13 +13,16 @@ function errorHandler(error)    {
     console.log("error occured", error)
     alert("something wrong with server! try again after sometime")
 }
+
 function clickEventHandler() {
-    // outputDiv.innerText = "translated!!!" +  txtInput.value
     var inputText = txtInput.value; //taking input
     // calling server for processing
-    fetch(getTranslationURL(serverURL+"?"+inputText))
+    fetch(getTranslationURL(inputText))
     .then(response => response.json())
-    .then(json => console.log(json.contents.translated))
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
     .catch(errorHandler)
 };
 
